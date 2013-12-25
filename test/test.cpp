787a077922441305d16588e680db7ce61bef0e05
tcpie <contact@tcpie.eu>
2013-12-25 17:01:37 +0100

    Expanded the unittests to also test STDCALL and THISCALL functions. Known bug at this moment: Module's MemoryRegion gets corrupted by... something.

--- test/test.cpp
+++ test/test.cpp
@@ -45,6 +45,24 @@ float __cdecl multiply(float a, int b)
 	return ret;
 }
 
+class test_class
+{
+public:
+	int add(int a, float b, int* ret_buffer)
+	{
+		int ret = a + (int)b;
+
+		*ret_buffer = ret;
+
+		return ret;
+	}
+
+	int add_end(int a, float b, int* ret_buffer)
+	{
+		return 0;
+	}
+};
+
 int __cdecl add(int a, float b, int* ret_buffer)
 {
 	int ret = a + (int)b;
@@ -53,6 +71,19 @@ int __cdecl add(int a, float b, int* ret_buffer)
 
 	return ret;
 }
+
+void add_end() { }
+
+int __stdcall add_stdcall(int a, float b, int* ret_buffer)
+{
+	int ret = a + (int)b;
+
+	*ret_buffer = ret;
+
+	return ret;
+}
+
+void add_stdcall_end() { }
 
 DetourRet __stdcall add_change_arg(DetourArgs* args)
 {
@@ -86,59 +117,146 @@ public:
 	}
 };
 
+typedef int (test_class::*test_memfnptr)(int, float, int*);
+
 bool hook_test()
 {
 	cout << "Hook test:" << endl;
-	vector<BYTE>* signature = new std::vector<BYTE>((BYTE*)add, (BYTE*)add_change_arg);
-	vector<char>* mask = new std::vector<char>(signature->size(), 'x');
 
-	Function* target = Function::FindFunction(signature,
-		mask,
-		CDECL_CALLCONV,			// Calling convention
-		3,						// Number of arguments
-		DWORD_SIZE);			// Return type
+	for (int i = CDECL_CALLCONV; i < 3; i ++)
+	{
+		void* fn = NULL;
+		void* fn_end = NULL;
+		test_class instance;
 
-	cout << "\tFindFunction() succeeded? " << (bool)(target != NULL) << endl;
+		if (i == CDECL_CALLCONV)
+		{
+			cout << "\t--- CDECL" << endl;
+			fn = (void*)&add;
+			fn_end = (void*)&add_end;
+		}
+		else if (i == STDCALL_CALLCONV)
+		{
+			cout << "\t--- STDCALL" << endl;
+			fn = (void*)&add_stdcall;
+			fn_end = (void*)&add_stdcall_end;
+		}
+		else if (i == THISCALL_CALLCONV)
+		{
+			cout << "\t--- THISCALL" << endl;
+			test_memfnptr fn_ptr = &test_class::add;
+			test_memfnptr fn_ptr_end = &test_class::add_end;
 
-	Hook* cdecl_hook = Hook::CreateHook(target, new wstring(L"add"));
+			__asm
+			{
+				push eax;
+				mov eax, fn_ptr;
+				mov fn, eax;
+				pop eax;
+			}
 
-	cout << "\tCreateHook() succeeded? " << (bool)(cdecl_hook != NULL) << endl;
+			__asm
+			{
+				push eax;
+				mov eax, fn_ptr_end;
+				mov fn_end, eax;
+				pop eax;
+			}
+		}
 
-	Detour* cdecl_change_arg_detour = cdecl_hook->RegisterDetour(add_change_arg, DETOUR_PRE);
+		vector<BYTE>* signature = new std::vector<BYTE>((BYTE*)fn, (BYTE*)fn_end);
+		vector<char>* mask = new std::vector<char>(signature->size(), 'x');
 
-	cout << "\tRegisterDetour(fn) succeeded? " << (bool)(cdecl_change_arg_detour != NULL) << endl;
+		Function* target = Function::FindFunction(signature,
+			mask,
+			(CallingConvention)i,	// Calling convention
+			3,						// Number of arguments
+			DWORD_SIZE);			// Return type
 
-	Detour* cdecl_class_detour = cdecl_hook->RegisterDetour(new detour_class(), DETOUR_PRE);
+		cout << "\tFindFunction() succeeded? " << (bool)(target != NULL) << endl;
 
-	cout << "\tRegisterDetour(class) succeeded? " << (bool)(cdecl_class_detour != NULL) << endl;
+		Hook* cdecl_hook = Hook::CreateHook(target, new wstring(L"add"));
 
-	Detour* cdecl_change_ret_detour = cdecl_hook->RegisterDetour(add_change_ret, DETOUR_POST);
+		cout << "\tCreateHook() succeeded? " << (bool)(cdecl_hook != NULL) << endl;
 
-	Detour* cdecl_blockfn = cdecl_hook->RegisterDetour(add_block_fn, DETOUR_PRE);
+		Detour* cdecl_change_arg_detour = cdecl_hook->RegisterDetour(add_change_arg, DETOUR_PRE);
 
-	cdecl_blockfn->Disable();
-	cdecl_hook->Enable();
-	cdecl_change_ret_detour->Disable();
+		cout << "\tRegisterDetour(fn) succeeded? " << (bool)(cdecl_change_arg_detour != NULL) << endl;
 
-	int ret_buffer = 0;
-	int ret = add(2, 5, &ret_buffer);
+		Detour* cdecl_class_detour = cdecl_hook->RegisterDetour(new detour_class(), DETOUR_PRE);
 
-	cdecl_class_detour->Disable();
+		cout << "\tRegisterDetour(class) succeeded? " << (bool)(cdecl_class_detour != NULL) << endl;
 
-	cout << "\tChange arg succeeded? " << (bool)(ret == 9) << endl;
-	cdecl_change_ret_detour->Enable();
-	cdecl_change_arg_detour->Disable();
+		Detour* cdecl_change_ret_detour = cdecl_hook->RegisterDetour(add_change_ret, DETOUR_POST);
 
-	ret = add(2, 5, &ret_buffer);
+		Detour* cdecl_blockfn = cdecl_hook->RegisterDetour(add_block_fn, DETOUR_PRE);
 
-	cout << "\tChange ret succeeded? " << (bool)(ret == 500) << endl;
-	cdecl_change_ret_detour->Disable();
+		cdecl_blockfn->Disable();
+		cdecl_hook->Enable();
+		cdecl_change_ret_detour->Disable();
 
-	ret_buffer = 0;
-	cdecl_blockfn->Enable();
-	ret = add(2, 5, &ret_buffer);
+		int ret_buffer = 0;
+		int ret = 0;
+		
+		switch (i)
+		{
+		case CDECL_CALLCONV:
+			ret = add(2, 5, &ret_buffer);
+			break;
 
-	cout << "\tBlock function succeeded? " << (bool)(ret_buffer == 0 && ret == 600) << endl;
+		case STDCALL_CALLCONV:
+			ret = add_stdcall(2, 5, &ret_buffer);
+			break;
+
+		case THISCALL_CALLCONV:
+			ret = instance.add(2, 5, &ret_buffer);
+			break;
+		}
+
+		cdecl_class_detour->Disable();
+
+		cout << "\tChange arg succeeded? " << (bool)(ret == 9) << endl;
+		cdecl_change_ret_detour->Enable();
+		cdecl_change_arg_detour->Disable();
+
+		switch (i)
+		{
+		case CDECL_CALLCONV:
+			ret = add(2, 5, &ret_buffer);
+			break;
+
+		case STDCALL_CALLCONV:
+			ret = add_stdcall(2, 5, &ret_buffer);
+			break;
+
+		case THISCALL_CALLCONV:
+			ret = instance.add(2, 5, &ret_buffer);
+			break;
+		}
+
+		cout << "\tChange ret succeeded? " << (bool)(ret == 500) << endl;
+		cdecl_change_ret_detour->Disable();
+
+		ret_buffer = 0;
+		cdecl_blockfn->Enable();
+		
+		switch (i)
+		{
+		case CDECL_CALLCONV:
+			ret = add(2, 5, &ret_buffer);
+			break;
+
+		case STDCALL_CALLCONV:
+			ret = add_stdcall(2, 5, &ret_buffer);
+			break;
+
+		case THISCALL_CALLCONV:
+			ret = instance.add(2, 5, &ret_buffer);
+			break;
+		}
+
+		cout << "\tBlock function succeeded? " << (bool)(ret_buffer == 0 && ret == 600) << endl; 
+	}
 
 	cout << "Hook test done. " << endl;
 
